a14f0b19915d1ead76235499f9a23600dc164e3a
LittleBigB <90601913+LittleBigB@users.noreply.github.com>
2021-11-22 17:30:09 +0900

    Add files via upload

---
+++ src_Jeonghyeon/components/Input.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, TextInput, Dimensions } from 'react-native';
+import { theme } from '../theme';
+
+const Input = ({ value, onChangeText, onSubmitEditing, onBlur }) => {
+    return (
+        <TextInput style = {inputStyle.textInput}
+            placeholder="+ Add a task"
+            placeholderTextColor= {theme.main}
+            maxLength={20}
+            value={value} onChangeText={onChangeText} 
+            onSubmitEditing={onSubmitEditing}
+            onBlur={onBlur}>
+        </TextInput>
+    );
+};
+
+const inputStyle = StyleSheet.create({     //처음부터 얘가 있다기 보단 category 옆 +버튼을 누르면 보이는 걸루
+    textInput: {
+        fontSize: 20,
+        width: Dimensions.get('window').width-30,
+        height: 30,
+        marginTop: 10,
+        marginLeft: 3,
+        paddingLeft: 15,
+        paddingTop: 2,
+        borderRadius: 0,
+        backgroundColor: theme.itemBackground,
+        color: theme.text,
+    },
+    
+});
+
+export default Input;
---
+++ src_Jeonghyeon/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+    background: '#ffffff',  //white
+    itemBackground: '#999999',  //i don't know
+    main: '#000000', //black
+    text: '#000000',
+    done: '#616161',    //i don't know
+};
