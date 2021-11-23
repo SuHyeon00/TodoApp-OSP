4642e040ceb5399588f68eb9bda21a5727c93826
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-11-24 01:03:17 +0900

    Delete src directory

--- src/src/theme.js
+++
@@ -1,9 +0,0 @@
-export const theme = {
-    background: '#ffffff',
-    itemBackground: '#FFFCFA',
-    main: '#6B5A48',
-    text: '#101010',
-    done: '#616161',
-    lightGreen: '#9EAB9A',
-    Green: '#879183',
-}
--- src/theme.js
+++
@@ -1,7 +0,0 @@
-export const theme = {
-    background: '#ffffff',  //white
-    itemBackground: '#9EAB9A',  //light green
-    main: '#6B5A48', //main color: brown
-    text: '#101010',  //black
-    done: '#616161',    //i don't know
-};
