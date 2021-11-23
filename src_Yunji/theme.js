d244bc5abb9020977af085d4aa17159055acefab
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-11-24 01:04:39 +0900

    Add files via upload

---
+++ src_Yunji/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    background: '#ffffff',
+    itemBackground: '#FFFCFA',
+    main: '#6B5A48',
+    text: '#101010',
+    done: '#616161',
+    lightGreen: '#9EAB9A',
+    Green: '#879183',
+}
