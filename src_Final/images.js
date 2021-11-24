d34a61900e2ce670fcc1e08fd23473b2ef341ed3
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-11-24 14:50:42 +0900

    Update images.js

--- src_Final/images.js
+++ src_Final/images.js
@@ -2,10 +2,14 @@ import UncheckedBox from '../assets/UncheckedBox.png';
 import CheckedBox from '../assets/CheckedBox.png';
 import Delete from '../assets/delete.png';
 import Edit from '../assets/edit.png';
+import addCategory from '../assets/folder-plus.png';
+import plus from '../assets/plus.png';
 
 export const images = {
     uncompleted: UncheckedBox,
     completed: CheckedBox,
     delete: Delete,
     update: Edit,
+    addCategory: addCategory,
+    plut: plus,
 };
