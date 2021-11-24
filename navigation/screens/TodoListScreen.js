483d4b9f0a6a96f78cfe9758ebcfb93f0f381f43
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-11-25 04:42:04 +0900

    Update TodoListScreen.js

--- navigation/screens/TodoListScreen.js
+++ navigation/screens/TodoListScreen.js
@@ -2,11 +2,8 @@ import * as React from 'react';
 import { StatusBar, SafeAreaView, Text, View, ScrollView } from 'react-native';
 import {viewStyles, textStyles, barStyles} from '../../src_Final/styles';
 import CalendarWeek from '../../src_Final/components/CalendarWeek';
-import TaskList from '../../src_Final/components/TaskList';
 import ScheduleList from '../../src_Final/components/ScheduleList';
-import { images } from '../../src_Final/images';
-import IconButton from '../../src_Final/components/IconButton';
-import { theme } from '../../src_Final/theme';
+import AddCategory from '../../src_Final/components/AddCategory';
 
 export default function TodoListScreen({navigation}) {
 
@@ -15,7 +12,7 @@ export default function TodoListScreen({navigation}) {
             <StatusBar style={barStyles.statusbar} />
             {/* Calendar */}
             <CalendarWeek />
-            
+
             <ScrollView>
                 <View>
                     <Text style={textStyles.category}>-- Today's Schedule</Text>
@@ -26,21 +23,7 @@ export default function TodoListScreen({navigation}) {
                 <Text style={viewStyles.divisionLine}> </Text>
 
                 {/* Add a Category */}
-                <View style={{flexDirection: 'row', marginVertical: 5}}>
-                    <IconButton type = {images.addCategory} />
-                    <Text style={{
-                        fontSize: 18,
-                        color: theme.text,
-                        marginTop: 8,
-                    }}>Add a category</Text>
-                </View>
-
-                {/* Category List */}
-                <View>
-                    <Text style={textStyles.category}>-- Category 1</Text>
-                    {/* Todo Item */}
-                    <TaskList />
-                </View>
+                <AddCategory />
             </ScrollView>
             
             
