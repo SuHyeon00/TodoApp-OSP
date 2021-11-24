3f92688e7df047730b75c0c23727c3cc449cb220
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-11-25 04:49:41 +0900

    Update TaskList.js

--- src_Final/components/TaskList.js
+++ src_Final/components/TaskList.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 import { View, Dimensions } from 'react-native';
 import Input from '../../src_Final/components/Input';
-import Task from '../../src_Final/components/Task';
+import Item from './Item';
 
 const TaskList = () => {
     const width = Dimensions.get('window').width;
@@ -10,52 +10,23 @@ const TaskList = () => {
     // Managing Todo Item
     const [newTask, setNewTask] = useState('');
     const [tasks, setTasks] = useState({});
-    // add a task
-    const _addTask = () => {
-        const ID = Date.now().toString();
-        const newTaskObject = {
-            [ID]: {id: ID, text: newTask, completed: false},
-        };
-        setNewTask('');
-        setTasks({...tasks, ...newTaskObject});
-    };
-    // delete a task
-    const _deleteTask = id => {
-        const currentTasks = Object.assign({}, tasks);
-        delete currentTasks[id];
-        setTasks(currentTasks);
-    };
-    // check a completed task
-    const _toggleTask = id => {
-        const currentTasks = Object.assign({}, tasks);
-        currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-        setTasks(currentTasks);
-    };
-    // edit a task
-    const _updateTask = item => {
-        const currentTasks = Object.assign({}, tasks);
-        currentTasks[item.id] = item;
-        setTasks(currentTasks);
-    };
-
-    const _onBlur = () => {
-        setNewTask('');
-    };
-
-    const _handleTextChangeTask = text => {
-        setNewTask(text);
-    };
 
     return (
         <View>
-            <Input value={newTask} placeholder="+ Add a task"
-                onChangeText={_handleTextChangeTask} 
-                onSubmitEditing={_addTask} onBlur={_onBlur}/>
-            <View width={width-20}>
+            <Input 
+                value={newTask}
+                placeholder="+ Add a task"
+                newItem={newTask}
+                setNewItem={setNewTask}
+                items={tasks}
+                setItems={setTasks}
+                />
+
+            <View style={{marginBottom: 10}} width={width-20}>
                 {Object.values(tasks).reverse().map(item => (
-                    <Task key={item.id} item={item}
-                    deleteTask={_deleteTask} toggleTask={_toggleTask}
-                    updateTask={_updateTask} />
+                    <Item key={item.id} item={item} items={tasks}
+                    setItems={setTasks}
+                    placeholder="+ Add a task" />
                 ))}
             </View>
         </View> 
