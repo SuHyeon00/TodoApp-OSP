014b51a1ae99473137ae92fa479909df0508b5a7
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-11-25 04:47:22 +0900

    Update Input.js

--- src_Final/components/Input.js
+++ src_Final/components/Input.js
@@ -3,15 +3,35 @@ import {TextInput} from "react-native";
 import { inputStyle } from "../styles";
 import {theme} from "../theme";
 
-const Input = ({value, placeholder, onChangeText, onSubmitEditing, onBlur }) => {
+const Input = ({value, placeholder, newItem, setNewItem, items, setItems }) => {
+
+    // add a item
+    const _addItem = () => {
+        const ID = Date.now().toString();
+        const newItemObject = {
+            [ID]: {id: ID, text: newItem, completed: false},
+        };
+        setNewItem('');
+        setItems({...items, ...newItemObject});
+    };
+
+    const _onBlur = () => {
+        setNewItem('');
+    };
+
+    const _handleTextChange = text => {
+        setNewItem(text);
+    };
+
     return (
         <TextInput style={inputStyle.textInput}
-        placeholder={placeholder}
-        placeholderTextColor= {theme.main}
-        maxLength={20}
-        value={value} onChangeText={onChangeText}
-        onSubmitEditing={onSubmitEditing}
-        onBlur={onBlur}>
+            placeholder={placeholder}
+            placeholderTextColor= {theme.main}
+            maxLength={20}
+            value={value} 
+            onChangeText={_handleTextChange}
+            onSubmitEditing={_addItem}
+            onBlur={_onBlur}>
         </TextInput>
     );
 };
