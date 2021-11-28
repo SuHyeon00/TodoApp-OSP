43c360bfe14f4d4cbefbc870ed0c2fb43d96385b
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-11-28 23:50:14 +0900

    Add files via upload

--- src_Yunji/images.js
+++ src_Yunji/images.js
@@ -2,10 +2,12 @@ import UncheckedBox from '../assets/UncheckedBox.png';
 import CheckedBox from '../assets/CheckedBox.png';
 import Delete from '../assets/delete.png';
 import Edit from '../assets/edit.png';
+import Comment from '../assets/comments.png'; 
 
 export const images = {
     uncompleted: UncheckedBox,
     completed: CheckedBox,
     delete: Delete,
     update: Edit,
+    comment : Comment,
 };
