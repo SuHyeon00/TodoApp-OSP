261a277719fb5144425ed4687771e0d1fe490500
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2021-12-04 20:52:58 +0900

    Delete/Select all

--- src_Final/components/TaskList.js
+++ src_Final/components/TaskList.js
@@ -30,6 +30,46 @@ const TaskList = (categoryId) => {
         const loadedTasks = await AsyncStorage.getItem('tasks');
         setTasks(JSON.parse(loadedTasks || '{}'));
     };
+    
+    // delete all items 
+    // !!!!카테고리별 삭제 되도록 수정!!!!
+    const _deleteAll = id => {
+        Alert.alert(
+            'Warning',
+            'Are you sure to delete all?',
+            [
+              {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+
+              {text: 'OK', onPress: () => {
+                const currentItems = Object.assign({}, tasks);
+                var array = new Array();
+                for(var i=0;i<array.length;i++){
+                    array[i]=currentItems[id];
+                };
+                for(var i=0;i<array.length;i++){
+                    delete array[i];
+                }
+                //setItems(array);
+                _saveTasks(array);}}
+            ]
+        );
+    }
+    
+    // select all items
+    const _selectAll = () => {
+        const currentItems = Object.assign({}, tasks);
+        var arr = new Array();
+        for(var i=0; i<arr.length; i++){
+            arr[i]=currentItems[id];
+        };
+        for(var i=0;i<arr.length;i++){
+            arr[i]['completed']=true;
+        };
+        //setItems(arr);
+        _saveTasks(arr);
+    }
+
+
 
     return isReady? (
         <View>
@@ -41,6 +81,14 @@ const TaskList = (categoryId) => {
                 saveItems={_saveTasks}
                 categoryId={categoryId}
                 />
+            <View style={{flexDirection: 'row'}}>
+                <IconButton type={images.deleteAll} onPressOut={_deleteAll} />
+                <Text style={textStyles.comment}>delete all</Text>
+
+                <IconButton type={images.selectAll} onPressOut={_selectAll} />
+                <Text style={textStyles.comment}>select all</Text>
+            </View>
+
             <View style={{marginBottom: 10}} width={width-20}>
                 {Object.values(tasks).reverse().map(item => (
                     <Item key={item.id} item={item}
