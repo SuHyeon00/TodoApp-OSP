5bd219d37c1c6578936f69d81701c4844dc4f358
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-12-05 02:37:08 +0900

    Update TodoListScreen.js

--- navigation/screens/TodoListScreen.js
+++ navigation/screens/TodoListScreen.js
@@ -25,6 +25,7 @@ export default function TodoListScreen({navigation}) {
                 {/* Add a Category */}
                 <AddCategory />
             </ScrollView>
+            <ShareExample />
             
             
         </SafeAreaView>
