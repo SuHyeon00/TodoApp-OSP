564ae4d476f61cfce92ab64c1f34ce103c342ef5
SJ-Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2021-12-13 15:11:39 +0900

    Add files via upload

--- navigation/screens/CompletionRateScreen.js
+++ navigation/screens/CompletionRateScreen.js
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CompletionRate from '../../src_Final/components/CompletionRate';
 import { textStyles, viewStyles } from '../../src_Final/styles';
+import Reward from '../../src_Final/components/Reward';
 
 export default function CompletionRateScreen({navigation}) {
+    const width = Dimensions.get('window').width;
     return (
         <View style={viewStyles.container}>
             <Text style={textStyles.title}>- Today’s completion rate</Text>
-            <CompletionRate />
+            {/* <CompletionRate /> */}
             
-            <Text style={textStyles.title}>-- Weekly completion rate</Text>
-            <CompletionRate />
+            {/* <Text style={textStyles.title}>-- Weekly completion rate</Text>
+            <CompletionRate /> */}
 
-            <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={textStyles.title}>REWARD</Text>
+            <View style={{width: width-20, alignSelf: 'center', alignItems: 'center', justifyContent: 'center'}}>
+                <Reward />
             </View>
         </View>
     );
