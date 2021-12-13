0cfc83e0212b97c44d4804596a0e07afebb30b4b
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-12-13 14:36:39 +0900

    Add files via upload

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -1,12 +1,34 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import IconButton from '../../src_Final/components/IconButton';
+import { images } from '../../src_Final/images';
+import { textStyles, viewStyles } from '../../src_Final/styles';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import TimeSetting from '../../src_Final/components/TimeSetting';
+import AccountModal from '../../src_Final/components/AccountModal';
+import RewardsModal from '../../src_Final/components/RewardsModal';
 
 export default function SettingsScreen({navigation}) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Todo List')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Settings Screen</Text>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Text onPress={() => navigation.navigate('Todo List')} />
+
+            <View style={viewStyles.container}>
+                <View style={{flexDirection: 'row'}}>
+                    <Image style={{ width: 80, height: 80, marginLeft: 5, marginRight:20, marginTop: 20,}} source={images.profile}/>
+                    <Text style={textStyles.account}>ME</Text>
+                </View>
+
+                <AccountModal/>
+
+                <TimeSetting/>
+
+                <RewardsModal/>
+
+
+            </View>
+
         </View>
     );
 }
