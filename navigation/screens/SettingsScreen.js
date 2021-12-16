a2dd0451eb78e6644b20a888330144138d50607e
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-12-16 19:50:48 +0900

    Add files via upload

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -2,37 +2,28 @@ import * as React from 'react';
 import { View, Text, Image } from 'react-native';
 import { images } from '../../src/images';
 import { textStyles, viewStyles } from '../../src/styles';
-import TimeSetting from '../../src/components/TimeSetting';
 import AccountModal from '../../src/components/AccountModal';
 import RewardsModal from '../../src/components/RewardsModal';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react/cjs/react.development';
+import TimeModal from '../../src/components/TimeModal';
 
-// 2071051 Ha YunJi
-
-export default function SettingsScreen({navigation}) {
-
-    const [nickname, setNickname] = React.useState('USER');
-
-    const loadNick = React.useMemo(async () => {
-        const loadedNicknames = await AsyncStorage.getItem('nicknames');
-        setNickname(loadedNicknames);
-        console.log(loadedNicknames);
-    });
-
-    console.log(nickname);
-
+export default function SettingsScreen({navigation, nicknames}) {
+    
+    const [text, setText] = useState('');
+    const _onChangeText = text => setText(text);
+    
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
 
             <View style={viewStyles.container}>
                 <View style={{flexDirection: 'row'}}>
-                    <Image style={{ width: 70, height: 70, marginLeft: 5, marginRight:20, marginTop: 20, }} source={images.profile}/>
-                    <Text style={textStyles.account}>{nickname.split('"')[1]}</Text>
+                    <Image style={{ width: 80, height: 80, marginLeft: 5, marginRight:20, marginTop: 20,}} source={images.profile}/>
+                    
                 </View>
 
                 <AccountModal/>
 
-                <TimeSetting/>
+                <TimeModal/>
 
                 <RewardsModal/>
 
