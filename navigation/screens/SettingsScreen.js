2ff1c61052dd24158bab4a73c26c2f05636dedaf
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-12-16 22:02:03 +0900

    Add files via upload

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -4,32 +4,46 @@ import { images } from '../../src/images';
 import { textStyles, viewStyles } from '../../src/styles';
 import AccountModal from '../../src/components/AccountModal';
 import RewardsModal from '../../src/components/RewardsModal';
-import { useState } from 'react/cjs/react.development';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import TimeModal from '../../src/components/TimeModal';
+import AppLoading from 'expo-app-loading';
 
-export default function SettingsScreen({navigation, nicknames}) {
-    
-    const [text, setText] = useState('');
-    const _onChangeText = text => setText(text);
-    
-    return (
+// 2071051 Ha YunJi
+
+export default function SettingsScreen({navigation}) {
+
+    const [nickname, setNickname] = React.useState('USER');
+    const [isReady, setIsReady] = React.useState(false);
+
+    const _loadData = async () => {
+        const loadedNickname = await AsyncStorage.getItem('nickname');
+        setNickname(loadedNickname.split('"')[1] || 'USER');
+    };
+
+    return isReady? (
         <View style={{ flex: 1, justifyContent: 'center' }}>
 
             <View style={viewStyles.container}>
-                <View style={{flexDirection: 'row'}}>
-                    <Image style={{ width: 80, height: 80, marginLeft: 5, marginRight:20, marginTop: 20,}} source={images.profile}/>
-                    
+                <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Image style={{ width: 70, height: 70, marginLeft: 5, marginRight:20, marginTop: 20, }} source={images.profile}/>
+                    <Text style={textStyles.account}>{nickname}</Text>
                 </View>
 
-                <AccountModal/>
+                <AccountModal nickname={nickname} setNickname={setNickname}/>
 
                 <TimeModal/>
 
                 <RewardsModal/>
 
-
+                <Image style={{ width: 240, height: 150, marginVertical: 40, marginHorizontal: 64, }} source={images.Gro}/>
             </View>
 
+
         </View>
-    );
+    ) : (
+        <AppLoading
+        startAsync={_loadData} // //동작하는 동안 실행될 함수
+        onFinish={() => setIsReady(true)}   //완료되면 실행할 함수
+        onError={console.error} />  //오류 발생시 실행할 함수
+  );
 }
