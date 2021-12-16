4719364c0b459ab70d244352a100fa8d403c2f6b
Yunji Ha <67634926+yunji118@users.noreply.github.com>
2021-12-16 15:59:39 +0900

    Add files via upload

---
+++ src/components/AccountModal.js
@@ -0,0 +1,127 @@
+import * as React from 'react';
+import { View, Text, Image, SafeAreaView, StyleSheet, TextInput, Alert, Modal, Pressable, Dimensions, Button } from "react-native";
+import { useState } from 'react/cjs/react.development';
+import { textStyles, ModalStyles } from '../styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { images } from '../images';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading';
+
+
+const AccountModal = () => {
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [isReady, setIsReady] = React.useState(false);
+
+   
+    const [emails, setEmails] = React.useState({});
+    const [nicknames, setNicknames] = React.useState({});
+
+    /*email 저장*/
+    const _saveEmail = async emails => {
+      try {
+          await AsyncStorage.setItem('emails' , JSON.stringify(emails));
+          setEmails(emails);
+      } catch (e) {
+          console.error(e);
+      }
+    };
+
+    /*nickname 저장*/
+    const _saveNickname = async () => {
+      try {
+          await AsyncStorage.setItem("nicknames" , JSON.stringify(nicknames));
+          setNicknames(nicknames);
+      } catch (e) {
+          console.error(e);
+      }
+      
+    };
+
+    const _loadData = async () => {
+      const loadedEmails = await AsyncStorage.getItem('emails');
+      setEmails(JSON.parse(loadedEmails || '{}'));
+
+      const loadedNicknames = await AsyncStorage.getItem('nicknames');
+      setNicknames(JSON.parse(loadedNicknames || '{}'));
+
+    };
+
+    
+    /* 버튼 누르면저장*/
+    const handleConfirm = () => {
+      _saveEmail(emails);
+      _saveNickname(nicknames);
+      setModalVisible(!modalVisible)      
+    };
+  
+
+  return isReady? (
+    <SafeAreaView style={{marginTop: 40}}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+      <View style={ModalStyles.modalView}>
+        <Text style={ModalStyles.modalText}>Account Settings</Text>
+        <View style={{flexDirection:'row'}}>
+          <Text style={{
+                fontSize:17,
+                marginTop: 17,
+                marginLeft: 35,
+                fontWeight: "bold",
+            }}>Email</Text>
+            <TextInput style={ModalStyles.input} 
+                      value={emails}
+                      onChangeText={(value)=>{setEmails(value);}}
+                      placeholder="Enter your email address"
+                      keyboardType="email-address"
+                      maxLength={40}                
+                      ></TextInput>
+        </View>
+          <View style={{flexDirection:'row'}}>
+              <Text style={{
+                  fontSize:17,
+                  marginTop: 17,
+                  fontWeight: "bold",
+              }}>Nickname</Text>
+              <TextInput
+                  style={ModalStyles.input}
+                  value={nicknames}
+                  onChangeText={(value)=>{setNicknames(value);}}
+                  placeholder="Enter your nickname"
+                  maxLength={40}
+              />
+          </View>
+            <Pressable
+              style={[ModalStyles.okbutton, ModalStyles.buttonClose]}
+              onPress={handleConfirm}>
+              <Text style={ModalStyles.textStyle}>OK</Text>
+            </Pressable>
+        </View>
+      </Modal>
+
+      <Pressable
+        style={[ModalStyles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+          <View style={{flexDirection:'row'}}>
+            <Image style={{ width: 30, height: 30, marginLeft: 15, marginRight:20, marginTop: 30,}} source={images.setting}/>
+            <TouchableOpacity><Text style={textStyles.title}>Account Settings</Text></TouchableOpacity>
+        </View>
+       
+      </Pressable>
+        
+    </SafeAreaView>
+  ) : (
+        <AppLoading
+        startAsync={_loadData} // //동작하는 동안 실행될 함수
+        onFinish={() => setIsReady(true)}   //완료되면 실행할 함수
+        onError={console.error} />  //오류 발생시 실행할 함수
+  );
+};
+
+export default AccountModal;
---
+++ src/components/RewardsModal.js
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import { View, Text, Image, SafeAreaView, StyleSheet, TextInput, Alert, Modal, Pressable } from "react-native";
+import { useState } from 'react/cjs/react.development';
+import { textStyles, ModalStyles } from '../styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { images } from '../images';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading';
+
+const RewardsModal = () => {
+  
+    const [modalVisible, setModalVisible] = useState(false);
+    const [isReady, setIsReady] = React.useState(false);
+
+    const [rates, setRates] = useState({});
+
+    /*reward rate 저장*/
+    const _saveRate = async rates => {
+      try {
+          await AsyncStorage.setItem('rates', JSON.stringify(rates));
+          setRates(rates);
+      } catch (e) {
+          console.error(e);
+      }
+    };
+
+    const _loadRates = async () => {
+      const loadedRates = await AsyncStorage.getItem('rates');
+      setRates(JSON.parse(loadedRates || '{}'));
+    }
+
+    /* 버튼 누르면저장*/
+    const handleConfirm = () => {
+      _saveRate(rates); 
+      setModalVisible(!modalVisible)      
+    };
+
+  return isReady? (
+    <SafeAreaView style={{marginTop: 40}}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={ModalStyles.modalView}>
+          <Text style={ModalStyles.modalTextTitle}>Rewards Settings</Text>
+          <View style={{flexDirection:'row'}}>
+              <Text style={styles.texts}>Completion rate</Text>
+
+              <TextInput
+                  style={styles.input}
+                  value={rates}
+                  onChangeText={(value)=>{setRates(value);}}
+                  keyboardType="number-pad"
+                  maxLength={3}>
+                  </TextInput>
+              
+                                    
+              <Text style={styles.texts}>%</Text>
+          </View>
+
+          <Pressable
+            style={[ModalStyles.okbutton, ModalStyles.buttonClose]}
+            onPress={handleConfirm}>
+            <Text style={ModalStyles.textStyle}>OK</Text>
+          </Pressable>
+        </View>
+      </Modal>
+      <Pressable style={[ModalStyles.buttonOpen]} onPress={() => setModalVisible(true)}>
+        <View style={{flexDirection:'row'}}>
+          <Image style={{ width: 30, height: 30, marginLeft: 15, marginRight:20, marginTop: 30,}} source={images.star}/>
+          <TouchableOpacity><Text style={textStyles.title}>Rewards Settings</Text></TouchableOpacity>
+        </View>
+      </Pressable>  
+    </SafeAreaView>
+  ) : (
+        <AppLoading
+        startAsync={_loadRates} 
+        onFinish={() => setIsReady(true)}
+        onError={console.error} />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: 45,
+    height: 40,
+    margin: 8,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+  texts:{
+      fontSize:17,
+      marginTop: 17,
+      fontWeight: "bold",
+      marginRight: 5,
+      marginLeft: 10,
+  }
+});
+
+export default RewardsModal;
