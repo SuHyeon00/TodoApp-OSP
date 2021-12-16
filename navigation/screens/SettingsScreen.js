ac2bc997f659c08cb2b815e4bea672f928ee872d
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-12-16 18:51:25 +0900

    Update SettingsScreen.js

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -5,15 +5,29 @@ import { textStyles, viewStyles } from '../../src/styles';
 import TimeSetting from '../../src/components/TimeSetting';
 import AccountModal from '../../src/components/AccountModal';
 import RewardsModal from '../../src/components/RewardsModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// 2071051 Ha YunJi
 
 export default function SettingsScreen({navigation}) {
+
+    const [nickname, setNickname] = React.useState('USER');
+
+    const loadNick = React.useMemo(async () => {
+        const loadedNicknames = await AsyncStorage.getItem('nicknames');
+        setNickname(loadedNicknames);
+        console.log(loadedNicknames);
+    });
+
+    console.log(nickname);
+
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
 
             <View style={viewStyles.container}>
                 <View style={{flexDirection: 'row'}}>
-                    <Image style={{ width: 80, height: 80, marginLeft: 5, marginRight:20, marginTop: 20,}} source={images.profile}/>
-                    <Text style={textStyles.account}>ME</Text>
+                    <Image style={{ width: 70, height: 70, marginLeft: 5, marginRight:20, marginTop: 20, }} source={images.profile}/>
+                    <Text style={textStyles.account}>{nickname.split('"')[1]}</Text>
                 </View>
 
                 <AccountModal/>
