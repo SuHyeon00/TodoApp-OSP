1e8d2c0c7d9384e4eb24432114fc58f3aad0cd6f
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-12-16 12:53:47 +0900

    Update MainContainer.js

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { theme } from '../src_Final/theme';
+import { theme } from '../src/theme';
 
 // Screens
 import CalendarScreen from './screens/CalendarScreen';
