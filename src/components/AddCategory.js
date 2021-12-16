66790eb7b3a42c2da9b9551b33301e79d2e7cd50
SuHyeon00 <90602694+SuHyeon00@users.noreply.github.com>
2021-12-16 12:22:18 +0900

    Category

---
+++ src/components/AddCategory.js
@@ -0,0 +1,61 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading';
+import * as React from 'react';
+import { useState } from 'react';
+import { View, Dimensions } from 'react-native';
+import Category from './Category';
+import Input from './Input';
+
+// 1928019 Oh SuHyeon
+
+const AddCategory = (selectedDate) => {
+    const width = Dimensions.get('window').width;
+
+    const [isReady, setIsReady] = React.useState(false);
+
+    // Managing Category
+    const [newCategory, setNewCategory] = useState('');
+    const [categories, setCategories] = useState({});
+
+    const _saveCategories = async categories => {
+        try {
+            await AsyncStorage.setItem('categories', JSON.stringify(categories));
+            setCategories(categories);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const _loadCategories = async () => {
+        const loadedCategories = await AsyncStorage.getItem('categories');
+        setCategories(JSON.parse(loadedCategories || '{}'));
+    }
+
+    return isReady? (
+        <View>
+            <Input
+                placeholder="+ Add a Category"
+                value={newCategory}
+                setNewItem={setNewCategory}
+                items={categories}
+                saveItems={_saveCategories}/>
+
+            <View width={width-20}>
+                {Object.values(categories).reverse().map(item => (
+                    <Category key={item.id} item={item}
+                    items={categories}
+                    saveItems={_saveCategories}
+                    placeholder="+ Add a Category"
+                    selectedDate={selectedDate}  />
+                ))}
+            </View>
+        </View> 
+    ) : (
+        <AppLoading 
+        startAsync={_loadCategories}
+        onFinish={() => setIsReady(true)}
+        onError={console.error} />
+    );
+}
+
+export default AddCategory;
---
+++ src/components/Category.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, Alert } from "react-native";
+import { theme } from "../theme";
+import IconButton from "./IconButton";
+import { images } from "../images";
+import { categoryStyle, textStyles } from "../styles";
+import TaskList from "./TaskList";
+import { TextInput } from "react-native-gesture-handler";
+
+// 1928019 Oh SuHyeon 1976086 Kim JeongHyeon
+
+const Category = ({ item, items, placeholder, saveItems, selectedDate }) => {
+
+    // delete a item 1976086 Kim JeongHyeon
+    
+    
+    // edit a item 1976086 Kim JeongHyeon
+    
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState(item.text);
+    
+    const _handleUpdateButtonPress = () => {
+        setIsEditing(true);
+    };
+
+    const _onSubmitEditing = () => {
+        if (isEditing) {
+            const editedItem = Object.assign({}, item, {text});
+            setIsEditing(false);
+        }
+    };
+
+    const _onBlur = () => {
+        if (isEditing) {
+            setIsEditing(false);
+            setText(item.text);
+        }
+    };
+
+    return isEditing ? (
+        <View style={{flexDirection: 'row'}}>
+                <IconButton type = {images.addCategory} style={categoryStyle.addIcon} />
+                <TextInput style={{fontSize: 20}}
+                    placeholder={placeholder}
+                    placeholderTextColor= {theme.main}
+                    maxLength={20}
+                    value={text}
+                    onChangeText={text => setText(text)}
+                    onSubmitEditing={_onSubmitEditing}
+                    onBlur={_onBlur}/>
+        </View>
+    ) : (
+        <View>
+            <View style={categoryStyle.container}>
+                <Text style ={textStyles.category}>-- {item.text}</Text>
+                <IconButton type={images.update} onPressOut={_handleUpdateButtonPress}/>
+                <IconButton type={images.delete} id={item.id} onPressOut={_deleteItem} />
+            </View>
+            
+            <TaskList categoryId={item.id} selectedDate={selectedDate}  />
+
+        </View>
+    )
+}
+
+export default Category;
